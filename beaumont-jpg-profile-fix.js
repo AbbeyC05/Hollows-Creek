6ac75764b5fd9c837279fc6cd158b2d39440a641
AbbeyC05 <corballyabbey@gmail.com>
2026-08-25 08:52:07 +0100

    Update selected Beaumont profile images to JPG

---
+++ beaumont-jpg-profile-fix.js
@@ -0,0 +1,30 @@
+(() => {
+  const jpgProfiles = {
+    'celia-profile.png': 'celia-profile.jpg',
+    'leon-profile.png': 'leon-profile.jpg',
+    'reece-profile.png': 'reece-profile.jpg',
+    'libby-profile.png': 'libby-profile.jpg',
+    'melissa-profile.png': 'melissa-profile.jpg'
+  };
+
+  function fixProfileImages(root = document) {
+    root.querySelectorAll('img').forEach(img => {
+      const src = img.getAttribute('src');
+      if (!src) return;
+      for (const [oldName, newName] of Object.entries(jpgProfiles)) {
+        if (src.includes(oldName)) {
+          img.setAttribute('src', src.replace(oldName, newName));
+          break;
+        }
+      }
+    });
+  }
+
+  fixProfileImages();
+  new MutationObserver(() => fixProfileImages()).observe(document.documentElement, {
+    childList: true,
+    subtree: true,
+    attributes: true,
+    attributeFilter: ['src']
+  });
+})();
