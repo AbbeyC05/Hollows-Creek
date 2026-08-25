eda341276d9e129d5d41427c5c170f42b1c08024
AbbeyC05 <corballyabbey@gmail.com>
2026-08-25 08:57:17 +0100

    Update Beaumont profile image extensions to JPEG

--- beaumont-jpg-profile-fix.js
+++ beaumont-jpg-profile-fix.js
@@ -4,7 +4,13 @@
     'leon-profile.png': 'leon-profile.jpg',
     'reece-profile.png': 'reece-profile.jpg',
     'libby-profile.png': 'libby-profile.jpg',
-    'melissa-profile.png': 'melissa-profile.jpg'
+    'melissa-profile.png': 'melissa-profile.jpg',
+    'lukas-profile.png': 'lukas-profile.jpeg',
+    'rosa-profile.png': 'rosa-profile.jpeg',
+    'lukas-jr-profile.png': 'lukas-jr-profile.jpeg',
+    'charlie-profile.png': 'charlie-profile.jpeg',
+    'bradley-profile.png': 'bradley-profile.jpeg',
+    'henry-profile.png': 'henry-profile.jpeg'
   };
 
   function fixProfileImages(root = document) {
